6c84331a5e3a9238dafb9d96149b641359a80630
MPalonek <mikepalonek@gmail.com>
2023-05-21 19:00:28 +0200

    Make logger thread safe

--- logger/src/logger.cpp
+++ logger/src/logger.cpp
@@ -2,18 +2,34 @@
 
 // Logger constructor
 // m_level is const, so once constructed it can't be changed
-Logger::Logger(logLevel l) : m_level(l)
+Logger::Logger(logLevel l, std::filesystem::path p) : m_level(l), m_filepath(p)
 {
 	std::stringstream ss;
-	ss << "Logger initialization. Level: " << logLevelStr[m_level] << " - " << serializeTimePoint(m_clock.now(), "%Y-%m-%d %H:%M:%S (%Z)");
+	ss << "Logger initialization...\n\tLevel: " << logLevelStr[m_level] << "\n\tDate: " << serializeTimePoint(m_clock.now(), "%Y-%m-%d %H:%M:%S (%Z)") << "\n\tLogPath: " << std::filesystem::absolute(m_filepath);
 	std::cout << ss.str() << std::endl;
+	m_queue.emplace_back(std::move(ss.str()));
+	//m_queue.resize(m_queueMaxSize);
+	m_file.open(m_filepath, std::ios::app); // std::ios::app for append mode
+	m_writerThread = std::thread(&Logger::writerLoop, this);
+}
+
+Logger::~Logger()
+{
+	addLog("Logger", "Logger shutting down...");
+	m_shutdown = true;
+	m_cv.notify_all();
+	if (m_writerThread.joinable())
+	{
+		m_writerThread.join();
+	}
+	m_file.close();
 }
 
 // Get Logger instance (Singleton)
 // LogLevel is set on first init for a lifetime!
-Logger& Logger::getInstance(logLevel level)
+Logger& Logger::getInstance(logLevel level, std::filesystem::path path)
 {
-	static Logger instance(level);
+	static Logger instance(level, path);
 	return instance;
 }
 
@@ -23,8 +39,12 @@ void Logger::addLog(std::string callerName, std::string msg, logLevel level)
 {
 	if (m_level <= level)
 	{
-		std::cout << m_clock.now() << "\n";
-		std::cout << serializeTimePoint(m_clock.now()) << " [" << logLevelStr[level] << "] " << callerName << ": " << msg << std::endl;
+		// lock will be locked only once on construction and unlocked on destruction
+		std::lock_guard lock(m_queueMutex);
+		std::string log = serializeTimePoint(m_clock.now()) + " [" + logLevelStr[level] + "] " + callerName + ": " + msg;
+		m_queue.emplace_back(std::move(log));
+		std::cout << m_queue.back() << std::endl;
+		m_cv.notify_one();
 	}
 }
 
@@ -38,10 +58,10 @@ std::string Logger::serializeTimePoint(const std::chrono::system_clock::time_poi
 	std::time_t tt = std::chrono::system_clock::to_time_t(time);
 	std::tm tm = *std::localtime(&tt); // Locale time-zone
 
-	m_sstream.str(""); // Clear the existing content
-	m_sstream << std::put_time(&tm, "%H:%M:%S");
-	m_sstream << '.' << std::setfill('0') << std::setw(3) << ms.count();
-	return m_sstream.str();
+	std::stringstream sstream;
+	sstream  << std::put_time(&tm, "%H:%M:%S");
+	sstream << '.' << std::setfill('0') << std::setw(3) << ms.count();
+	return sstream.str();
 }
 
 // Turns std::chrono::system_clock::time_point into string, according to passed format.
@@ -55,9 +75,36 @@ std::string Logger::serializeTimePoint(const std::chrono::system_clock::time_poi
 	//std::tm tm = *std::gmtime(&tt); // GMT (UTC)
 	std::tm tm = *std::localtime(&tt); // Locale time-zone
 
-	m_sstream.str(""); // Clear the existing content
-	m_sstream << std::put_time(&tm, format.data());
-	return m_sstream.str();
+	std::stringstream sstream;
+	sstream  << std::put_time(&tm, format.data());
+	return sstream.str();
+}
+
+void Logger::writerLoop()
+{
+	while (true)
+	{
+		// Wait until main() sends data
+		// cv.wait _Predicate if true, then thread wakes up  
+		std::unique_lock lock(m_queueMutex);
+		m_cv.wait(lock, [&] {
+			return (!m_queue.empty() || m_shutdown); });
+
+		if (m_shutdown)
+		{
+			return;
+		}
+		m_file << m_queue.front() << std::endl;
+		m_queue.pop_front();
+	}
+}
+
+void helperfunc(std::string str)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		Logger::getInstance().addLog(str, "Some random string__" + std::to_string(i));
+	}
 }
 
 int main()
@@ -67,5 +114,21 @@ int main()
 	Logger::getInstance(Logger::logLevel::DEBUG).addLog("main", "info message");
 	Logger::getInstance().addLog("main", "warning message", Logger::WARNING);
 	Logger::getInstance().addLog("main", "debug message", Logger::DEBUG);
+
+	std::this_thread::sleep_for(std::chrono::seconds(4));
+
+	for (int i = 0; i < 50; i++)
+	{
+		std::string str = "thread_" + std::to_string(i);
+		std::thread([str] { helperfunc(str); }).detach();
+	}
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+	// performance:
+	// 1st run		18:34:38.445 - 18:34:38.272 = 173
+	// 2nd run		18:42:36.322 - 18:42:36.150 = 172
+	// 3rd run		18:45:55.266 - 18:45:55.093 = 173
+	// 50 threads x 10 logs = 500 logs
+	// 500 logs / 173 ms
+	// 1 log / 0.346 ms (avg)
 
 }
--- logger/src/logger.h
+++ logger/src/logger.h
@@ -3,8 +3,32 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 #include <chrono>
 #include <array>
+#include <deque>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+
+/*
+To make a fast logger you need to:
+
+	minimise the use of mutexes.
+	detach the logging from the writing.
+	minimise copying
+	pre-alloc the buffers
+		no allocs during logging.
+	make a ring buffer
+		fixed number of logging
+		decide what should happen if its full
+		slightly slower using a dequeue with minimises allocs
+	use atomic indexes to the (ring) buffer
+		if your system if fully loaded, you have to log often use the cv or wait until the logger thread gets scheduled by the OS.
+		use the cv.notice_one where the index is updated if the front is sufficient in front of the back end.
+*/
 
 
 class Logger
@@ -31,26 +55,29 @@ public:
 	void operator=(Logger&&) = delete;
 
 	// actual functions
-	static Logger& getInstance(logLevel level = INFO);
+	static Logger& getInstance(logLevel level = INFO, std::filesystem::path path = "log.txt");
 	void addLog(std::string callerName, std::string msg, logLevel level = INFO);
 
 private:
-	Logger(logLevel l);
-	~Logger() = default;
+	Logger(logLevel l, std::filesystem::path p);
+	~Logger();
 
 	std::string serializeTimePoint(const std::chrono::system_clock::time_point& time);
 	std::string serializeTimePoint(const std::chrono::system_clock::time_point& time, std::string_view format);
-	
+	void writerLoop();
 
 	std::chrono::system_clock m_clock;
-	std::stringstream m_sstream; // shared var...
-	const std::string m_filepath; // TODO
+	std::ofstream m_file;
+	const std::filesystem::path m_filepath;
 	const logLevel m_level;
 
-	// thread
-	// queue
-	// cond_var
-	// mutex
+	std::deque<std::string> m_queue;
+	const size_t m_queueMaxSize = 10; //TODO not used currently
+	std::mutex m_queueMutex;
+
+	std::thread m_writerThread;	// thread
+	std::condition_variable m_cv;
+	bool m_shutdown = false;
 	// flush_period
 
 };
